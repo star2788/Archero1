3e55ee9d9961480332f89b26ae282afbeb828489
star2788 <star2788@naver.com>
2025-02-20 19:27:11 +0900

    20250220_Archero_mobile
    Assets
    유니티 에셋 추가
     1. 조이스틱
     2. 플레이어 캐릭터 모델링 및 애니메이션
     3. 몬스터 모델링
     4. 활 모델링
     5. 맵 구축을 위한 환경 모델링
    
    Scenes
    간단한 맵 생성
    
    Scripts
    조이스틱을 통한 플레이어 이동 구현

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.EventSystems;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour{
+    //조이스틱 에셋
+    public Joystick joystick;
+    //플레이어 이동 속도
+    public float moveSpeed = 5f;
+
+    //플레이어의 리지드바디
+    private Rigidbody playerRigidbody;
+
+    void Start() {
+        //플레이어 리지드바디 컴포넌트 가져오기
+        playerRigidbody = GetComponent<Rigidbody>();
+    }
+
+
+
+    private void FixedUpdate() {
+        Rotate();
+        Move();
+
+       
+    }
+
+    private void Move() {
+        //조이스틱의 입력 값을 벡터로 받음
+        Vector3 direction = new Vector3(joystick.Direction.x, 0 , joystick.Direction.y);
+        //입력 값을 통한 플레이어 이동
+        playerRigidbody.velocity = direction * moveSpeed;
+       
+    }
+
+    private void Rotate() {
+        //조이스틱의 방향 각도를 구해서 회전값을 구함.
+        float turn = Mathf.Atan2(joystick.Direction.x, joystick.Direction.y) * Mathf.Rad2Deg;
+        //조이스틱의 방향 입력 값이 있을 경우에만 회전
+        if (joystick.Direction.x != 0 || joystick.Direction.y != 0) {
+            transform.rotation = Quaternion.Euler(0, turn, 0);
+        }
+    }
+
+}
